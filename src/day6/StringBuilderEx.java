696f3d8bf4f9394f3ece51c3897e3e2b28656c84
Daksh <dakshahuja31106@gmail.com>
2026-08-25 12:32:35 +0530

    session 2 file

---
+++ src/day6/StringBuilderEx.java
@@ -0,0 +1,13 @@
+package day6;
+
+public class StringBuilderEx {
+	    public static void main(String[] args) {
+	        StringBuilder builder = new StringBuilder();
+
+	        builder.append("Hello");
+	        builder.append(" ");
+	        builder.append("world!");
+
+	        System.out.println(builder.toString());
+	    }
+	}
